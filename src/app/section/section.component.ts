797ccc92df64248bc9f8209cd223658778cb9bef
Daniel <fernandez.sanchez.palencia.daniel@gmail.com>
2020-01-30 11:29:03 +0100

    Bucles ngFor y condicional ngIf

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -7,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionComponent implements OnInit {
 
+  // Array de tipo string
+  cursos: string[] = ['HTML5', 'CSS3', 'SASS', 'Java8'];
+
+  // Array de objetos animales
+  animales:Array<any> = [
+    {especie: 'perro', nombre: 'Lucia', anios: 8, pelo: 'corto'},
+    {especie: 'gato', nombre: 'Felix', anios: 3, pelo: 'corto'},
+    {especie: 'gato', nombre: 'Lulu', anios: 5, pelo: 'largo'},
+    {especie: 'perro', nombre: 'Pipo', anios: 7, pelo: 'largo'}
+  ];
+  ocultar:boolean = true;
+  nBoton:string = 'Ocultar';
+
   constructor() { }
+
+  boton(){
+    if(this.ocultar == false){
+      this.nBoton = 'Mostrar';
+    }else{
+      this.nBoton = 'Ocultar';
+    }
+  }
 
   ngOnInit() {
   }
