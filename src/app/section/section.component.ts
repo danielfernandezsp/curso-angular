7a5adb5fe9a594a3da7759c271bfbfee544f7468
Daniel <fernandez.sanchez.palencia.daniel@gmail.com>
2020-01-31 13:16:57 +0100

    Guardado

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -11,26 +11,49 @@ export class SectionComponent implements OnInit {
   cursos: string[] = ['HTML5', 'CSS3', 'SASS', 'Java8'];
 
   // Array de objetos animales
-  animales:Array<any> = [
-    {especie: 'perro', nombre: 'Lucia', anios: 8, pelo: 'corto'},
-    {especie: 'gato', nombre: 'Felix', anios: 3, pelo: 'corto'},
-    {especie: 'gato', nombre: 'Lulu', anios: 5, pelo: 'largo'},
-    {especie: 'perro', nombre: 'Pipo', anios: 7, pelo: 'largo'}
+  animales: Array<any> = [
+    { especie: 'perro', nombre: 'Lucia', anios: 8, pelo: 'corto' },
+    { especie: 'gato', nombre: 'Felix', anios: 3, pelo: 'corto' },
+    { especie: 'gato', nombre: 'Lulu', anios: 5, pelo: 'largo' },
+    { especie: 'perro', nombre: 'Pipo', anios: 7, pelo: 'largo' }
   ];
-  ocultar:boolean = true;
-  nBoton:string = 'Ocultar';
+
+  estudiantes: Array<any> = [];
+  tablaEstudiantes: boolean = false;
+  ocultar: boolean = true;
+  nBoton: string = 'Ocultar';
 
   constructor() { }
 
-  boton(){
-    if(this.ocultar == false){
+  boton() {
+    if (this.ocultar == false) {
       this.nBoton = 'Mostrar';
-    }else{
+    } else {
       this.nBoton = 'Ocultar';
     }
   }
 
+
+  guardar() {
+    var nombre = (<HTMLInputElement>document.getElementById("nombre")).value;
+    var edad = (<HTMLInputElement>document.getElementById("edad")).value;
+    var curso = (<HTMLInputElement>document.getElementById("curso")).value;
+
+    this.estudiantes.push({ aNombre: nombre, aEdad: edad, aCurso: curso });
+
+    console.log(this.estudiantes);
+
+    if (this.estudiantes.length == 0) {
+      this.tablaEstudiantes = false;
+    } else {
+      this.tablaEstudiantes = true;
+    }
+
+  }
+
+
   ngOnInit() {
+
   }
 
 }
